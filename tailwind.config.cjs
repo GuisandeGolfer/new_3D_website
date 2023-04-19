795ca08ac20be047dbdca1986cf1a80033d0af4c
GuisandeGolfer <DGuisande873@gmail.com>
2023-04-18 18:45:15 -0700

    fix base url in vite.config.js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
@@ -25,3 +24,4 @@ module.exports = {
   },
   plugins: [],
 };
+/** @type {import('tailwindcss').Config} */
