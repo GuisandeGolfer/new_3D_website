10fdfcd08c3eb201218a7f5a60b9df206a4821c4
GuisandeGolfer <DGuisande873@gmail.com>
2023-04-20 13:46:17 -0700

    change base config again for vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://guisandegolfer.github.io/new_3D_website/",
+  base: "/new_3D_website/",
 });
