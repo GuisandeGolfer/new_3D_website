deaa4ed2734540fe9c41ed9fe8fa353efa4aa9cf
GuisandeGolfer <DGuisande873@gmail.com>
2023-05-11 23:21:04 -0700

    chore(Computers.jsx): fix mobile responsivity

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Diego</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a self-taught developer who is currently looking for
+            I am a self-taught developer who is currently looking for{" "}
             <br className="sm:block hidden" />
             a Junior Software Engineer role.
           </p>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -24,8 +24,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.45 : 0.75}
+        position={isMobile ? [0, -10, -2.2] : [0, -89.5, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
         // rotation={[-Math.PI / 2, 0, 0]}
       />
